0677b850e2d0e6618f65766b6233a660454c0fe5
HP <HP@Devesh>
2025-06-30 14:52:55 +0530

    variables and datatypes

---
+++ TNSIF_Training/src/DayTwo/Operators.java
@@ -0,0 +1,16 @@
+package DayTwo;
+
+public class Operators {
+	public static void main(String [] args) {
+		int a= 10;
+		int b =20;
+		System.out.println("A and B value before " + a + " and " + b);
+		
+		++a;
+		int c = ++a +b + a--;
+		System.out.println("C value after the operation : " + c);
+		
+		int d= c++ + a + b--;
+		System.out.println("D value after the operation : " + d);
+	}
+}
