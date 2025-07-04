7f8fdabae9a928e21c300e2a3d1f82019ba0a175
HP <HP@Devesh>
2025-07-04 11:29:41 +0530

    Day Six

---
+++ TNSIF_Training/src/Day_Six/Employee_Static_main.java
@@ -0,0 +1,8 @@
+package Day_Six;
+
+public class Employee_Static_main {
+	public static void main(String[] args) {
+		Emplpoyee_Static e = new Emplpoyee_Static("Devesh",123);
+		System.out.println(e);
+	}
+}
---
+++ TNSIF_Training/src/Day_Six/Emplpoyee_Static.java
@@ -0,0 +1,19 @@
+package Day_Six;
+
+public class Emplpoyee_Static {
+	private String name;
+	private int id;
+	
+	private static String companyName = "TNSIF";
+
+	public Emplpoyee_Static(String name, int id) {
+		this.name = name;
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Emplpoyee_Static [name=" + name + ", id=" + id + " ,CompanyName= "+companyName+ "]";
+	}
+	
+}		
