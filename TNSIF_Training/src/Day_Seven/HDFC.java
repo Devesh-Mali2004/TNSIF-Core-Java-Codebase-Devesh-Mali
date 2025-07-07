02e6a6ee88ba611e3233844ac085ae73dbbe49fa
HP <HP@Devesh>
2025-07-07 12:20:03 +0530

    Day 7

---
+++ TNSIF_Training/src/Day_Seven/HDFC.java
@@ -0,0 +1,8 @@
+package Day_Seven;
+
+public class HDFC extends RBI {
+	public int getRateOfInteretst() {
+		System.out.println("Base rate of interest for home loam");
+		return 43;
+	}
+}
---
+++ TNSIF_Training/src/Day_Seven/RBI.java
@@ -0,0 +1,8 @@
+package Day_Seven;
+
+public class RBI {
+	public int getRateOfInteretst() {
+		System.out.println("Base rate of interest for home loam");
+		return 6;
+	}
+}
