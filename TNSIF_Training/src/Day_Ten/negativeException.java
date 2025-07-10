ff4e6fd3268f5048063e69d07975a7fb078abe96
HP <HP@Devesh>
2025-07-10 11:20:27 +0530

    Day 10

---
+++ TNSIF_Training/src/Day_Ten/negativeException.java
@@ -0,0 +1,11 @@
+package Day_Ten;
+public class negativeException extends Exception{
+
+	public negativeException(String str) {
+		super(str);
+	}
+	public negativeException() {
+		super("percentage should not be negative");
+	}
+	
+}
